17f012b230a519610fc41232d752b189fa9912af
Yulia Khrenenko <romanova_jv@mail.ru>
2020-06-22 12:56:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5_110;
+        int stock = 20;
+        int miles = amount/stock;
+
+        System.out.println(miles);
+    }
+}
